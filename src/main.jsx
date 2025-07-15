b7b19cb9ca191aa3fa3b224bf6eb8ee3cb0b8297
sowmiya-rajendiran <sowmiraj910@gmail.com>
2025-07-15 15:42:55 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { FormProvider } from './Context/FormContext.jsx'
+import { FormProvider } from './context/FormContext.jsx'
 
 createRoot(document.getElementById('root')).render(
     <FormProvider>
